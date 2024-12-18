680998544caf1718451970758c03ea8e1fcafa02
Merakigai <merakigailabs@gmail.com>
2024-12-18 16:03:40 +0700

    ch4: end of chapter4

--- ch4-arrays-and-slices/sum.go
+++ ch4-arrays-and-slices/sum.go
@@ -7,3 +7,21 @@ func Sum(numbers []int) int {
 	}
 	return count
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	res := []int{}
+	for _, numbers := range numbersToSum {
+		res = append(res, Sum(numbers))
+	}
+	return res
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
--- ch4-arrays-and-slices/sum_test.go
+++ ch4-arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -22,7 +25,30 @@ func TestSum(t *testing.T) {
 
 }
 
+func TestSumAll(t *testing.T) {
+	t.Run("sums all slices of integers together", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		assertObjectEqual(t, got, want)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	assertObjectEqual(t, got, want)
+}
+
+func assertObjectEqual(t testing.TB, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
 func assertCorrectMessage(t testing.TB, want, got int, numbers []int) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
