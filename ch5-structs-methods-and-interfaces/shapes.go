1edd4b80d1105e7e11743b719b3f9eba2c480458
Merakigai <merakigailabs@gmail.com>
2024-12-18 21:20:17 +0700

    ch5: add Circle Struct and tests

--- ch5-structs-methods-and-interfaces/shapes.go
+++ ch5-structs-methods-and-interfaces/shapes.go
@@ -1,9 +1,28 @@
 package structsmethodsandinterfaces
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- ch5-structs-methods-and-interfaces/shapes_test.go
+++ ch5-structs-methods-and-interfaces/shapes_test.go
@@ -1,25 +1,62 @@
 package structsmethodsandinterfaces
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	want := 40.0
 
-	assertFloatEqual(t, got, want)
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Perimeter()
+		want := 40.0
+
+		assertFloatEqual(t, got, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Perimeter()
+		want := 62.83
+
+		assertFloatAlmostEqual(t, got, want)
+	})
+
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.00
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.00
 
-	assertFloatEqual(t, got, want)
+		assertFloatEqual(t, got, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.16
+
+		assertFloatAlmostEqual(t, got, want)
+	})
+
 }
 
 func assertFloatEqual(t testing.TB, got, want float64) {
 	t.Helper()
 
 	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+const float64EqualityThreshold = 1e-9
+
+func assertFloatAlmostEqual(t testing.TB, got, want float64) {
+	t.Helper()
+	if math.Abs(got-want) <= float64EqualityThreshold {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
