1e3d2a8d9d96073f8dbc2709e0fa51eeab551970
Merakigai <merakigailabs@gmail.com>
2024-12-19 14:18:03 +0700

    ch7: add unknown word

--- ch7-maps/dictionary.go
+++ ch7-maps/dictionary.go
@@ -1,7 +1,16 @@
 package maps
 
+import "errors"
+
+var ErrNotFound = errors.New("EXPECTED TO GET AN ERROR")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(key string) string {
-	return d[key]
+func (d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
