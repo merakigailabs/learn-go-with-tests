6f1abe140ea9515e017fea3211c99225ca60b89f
Merakigai <merakigailabs@gmail.com>
2024-12-19 11:29:24 +0700

    ch7: refactor to dictionnary

--- ch7-maps/dictionary.go
+++ ch7-maps/dictionary.go
@@ -1,5 +1,7 @@
 package maps
 
-func Search(dictionary map[string]string, key string) string {
-	return dictionary[key]
+type Dictionary map[string]string
+
+func (d Dictionary) Search(key string) string {
+	return d[key]
 }
