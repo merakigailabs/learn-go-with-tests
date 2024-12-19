d3d211452e2baffcf0374bdb561c30c2a197a931
Merakigai <merakigailabs@gmail.com>
2024-12-19 15:02:30 +0700

    ch7: implementation of the update function

--- ch7-maps/dictionary.go
+++ ch7-maps/dictionary.go
@@ -2,7 +2,11 @@ package maps
 
 import "errors"
 
-var ErrNotFound = errors.New("EXPECTED TO GET AN ERROR")
+var (
+	ErrNotFound         = errors.New("EXPECTED TO GET AN ERROR")
+	ErrWordExists       = errors.New("WORD EXISTS")
+	ErrWordDoesNotExist = errors.New("WORD DOES NOT EXIST")
+)
 
 type Dictionary map[string]string
 
@@ -17,6 +21,32 @@ func (d Dictionary) Search(key string) (string, error) {
 
 // An interesting property of maps is
 // that you can modify them without passing as an address to it
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = newDefinition
+	default:
+		return err
+	}
+
+	return nil
 }
--- ch7-maps/dictionary_test.go
+++ ch7-maps/dictionary_test.go
@@ -26,13 +26,50 @@ func TestSearch(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
 
-	word := "test"
-	definition := "this is just a test"
+		dictionary.Add(word, definition)
 
-	dictionary.Add(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
 
-	assertDefinition(t, dictionary, word, definition)
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
