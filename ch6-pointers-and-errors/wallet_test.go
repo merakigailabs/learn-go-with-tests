319127247b326c5efafd44be587ec7243d41a768
Merakigai <merakigailabs@gmail.com>
2024-12-19 10:24:26 +0700

    ch6: refactor assert helper

--- ch6-pointers-and-errors/wallet_test.go
+++ ch6-pointers-and-errors/wallet_test.go
@@ -4,28 +4,27 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit Bitcoin", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("Deposit Bitcoin", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Widthraw Bitcoin", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Widthraw(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 }
